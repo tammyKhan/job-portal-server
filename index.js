aab95ed0a3819fcc5f66516d74e206c1d4879791
tammykhan <tammykhan543@gmail.com>
2025-01-27 14:45:14 +0600

    use cookie parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9clsv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -35,8 +37,14 @@ async function run() {
   // Auth related api's
   app.post('/jwt', async (req, res) => {
     const user = req.body;
-    const token = jwt.sign(user, 'secret', {expiresIn: '1h'});
-    res.send(token);
+    const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1h'});
+    res
+    .cookie('token', token, {
+      httpOnly: true,
+      secure: false,
+
+    })
+    .send({success: true});
   })
 
   // jobs related API
