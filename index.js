3e9210ddff31f2d6c1aceb48051a90e1e5301aeb
tammykhan <tammykhan543@gmail.com>
2025-01-28 10:22:05 +0600

    set token to the cookie

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser())
 
@@ -76,11 +79,12 @@ async function run() {
   // job application apis
   // get all data
 
-
-
   app.get('/job-application', async (req,res) => {
     const email = req.query.email;
     const query = { applicant_email: email }
+
+    console.log('worst Day', req.cookies)
+
     const result = await jobApplicationCollection.find(query).toArray();
 
     // worst way to aggregate data
